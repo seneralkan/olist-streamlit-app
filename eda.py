3d7feba60ed2b0d7804a0f10910fff216518cc73
Sener Alkan <seneralkan77@gmail.com>
2021-12-03 21:25:21 +0300

    fix bug

--- eda.py
+++ eda.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd 
-import matplotlib
-matplotlib.use('Qt5Agg')
+# import matplotlib
+# matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
